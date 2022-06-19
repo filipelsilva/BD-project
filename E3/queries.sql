f3d9428c2eb103b362bc30defb392c8f4fb11461
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2022-06-19 01:08:49 +0100

    lowercase stuff is easier to write

--- E3/queries.sql
+++ E3/queries.sql
@@ -2,44 +2,44 @@
 -- categorias? 
 
 -- TODO: problema categorias em IVMs diferentes --
-SELECT name
-FROM responsavel_por NATURAL JOIN retalhista
-GROUP BY name  
-HAVING COUNT(DISTINCT nome_cat) >= ALL (
-    SELECT COUNT(DISTINCT nome_cat)
-    FROM responsavel_por NATURAL JOIN retalhista
-    GROUP BY name
+select name
+from responsavel_por natural join retalhista
+group by name  
+having count(distinct nome_cat) >= all (
+    select count(distinct nome_cat)
+    from responsavel_por natural join retalhista
+    group by name
 );   
 
 
 -- Qual o nome do ou dos retalhistas que sao responsaveis por todas as categorias simples?
 
 -- TODO: ?? not sure
-SELECT name
-FROM responsavel_por NATURAL JOIN retalhista
-GROUP BY nome
-HAVING COUNT(DISTINCT nome_cat) = (
-    SELECT COUNT(*)
-    FROM categoria_simples
+select name
+from responsavel_por natural join retalhista
+group by nome
+having count(distinct nome_cat) = (
+    select count(*)
+    from categoria_simples
 );
 
 -- Quais os produtos (ean) que nunca foram repostos?
-SELECT ean
-FROM produto 
-WHERE ean NOT IN (
-    SELECT ean FROM evento_reposicao)
+select ean
+from produto 
+where ean not in (
+    select ean from evento_reposicao)
 ;
 
 -- Quais os produtos (ean) que foram repostos sempre pelo mesmo retalhista?
 -- TODO: ??? o que eu fiz esta no slide 46 de SQL avancado 2
-SELECT ean
-FROM evento_reposicao a
-WHERE NOT EXISTS(
-    SELECT ean 
-    FROM evento_reposicao
-    EXCEPT
-    SELECT b.ean
-    FROM (evento_reposicao b)
-    WHERE a.tin = b.tin
+select ean
+from evento_reposicao a
+where not exists(
+    select ean 
+    from evento_reposicao
+    except
+    select b.ean
+    from (evento_reposicao b)
+    where a.tin = b.tin
 )
 ;
