88782edc289f01eb9937876a9e65b0b38d48d80f
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2022-06-20 21:44:50 +0100

    view

--- E3/view.sql
+++ E3/view.sql
@@ -1,14 +1,14 @@
-create view Vendas(ean, cat, ano, trimestre, mes, dia_mes, dia_semana, distrito, conselho, unidades)
+create view Vendas(ean, nome_categoria, ano, trimestre, mes, dia_mes, dia_semana, distrito, concelho, unidades)
 as
 select p.ean as ean,
-       p.cat as cat,
+       p.nome_categoria as nome_categoria,
        extract(year from e.instante) as ano,
        extract(quarter from e.instante) as trimestre,
        extract(month from e.instante) as mes,
        extract(day from e.instante) as dia_mes,
        extract(dow from e.instante) as dia_semana,
        r.distrito as distrito,
-       r.conselho as conselho,
+       r.concelho as concelho,
        e.unidades as unidades
 from evento_reposicao e
     join produto p
@@ -16,5 +16,5 @@ from evento_reposicao e
     join instalada_em i
         on i.num_serie = e.num_serie and i.fabricante = e.fabricante
     join ponto_de_retalho r
-        on r.nome = i.local
+        on r.nome_ponto_de_retalho = i.local
 ;
