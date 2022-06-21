7b6c2c4cb81b27dadbc15d4135a6eee320d50a97
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2022-06-21 15:16:48 +0100

    fixed olaps

--- E3/analytics.sql
+++ E3/analytics.sql
@@ -2,19 +2,22 @@
 --        6. OLAP         --
 ----------------------------
 
--- 1. num dado periodo (i.e. entre duas datas), por dia da semana, por concelho
--- e no total
+-- 1. num dado periodo (i.e. entre duas datas), por dia da semana,
+-- por concelho e no total
 
 -- TODO: falta periodo de tempo. nao sei como fazer com datas desconhecidas
 
-select dia_semana, conselho, SUM(unidades) as total
-from Vendas
-group by CUBE(dia_semana,conselho);
+select dia_semana, concelho, sum(unidades) as total
+from vendas
+where ano >= '2020' and ano <= '2022'
+	and mes >= '1' and mes <= '10'
+	and dia_mes >= '10' and dia_mes <= '31'
+group by cube(dia_semana, concelho);
 
--- 2. num dado distrito (i.e. "Lisboa"), por concelho, categoria, dia da semana
--- e no total
+-- 2. num dado distrito (i.e. "Lisboa"), por concelho, categoria, dia
+-- da semana e no total
 
-select conselho, cat, dia_semana, SUM(unidades) as total
-from Vendas
-where distrito = "Lisboa"
-group by CUBE(conselho, cat, dia_semana);
+select concelho, nome_categoria, dia_semana, sum(unidades) as total
+from vendas
+where distrito = 'Leiria'
+group by cube(concelho, nome_categoria, dia_semana);
